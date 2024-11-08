a1f011c5dbe5c83b9b18fa9d83bbca080145aa38
Amelia French <126724144+ameliafrench08@users.noreply.github.com>
2024-11-08 10:49:39 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         int popNumber = 0;
         int rockNumber = 0;
         int classicalNumber = 0;
@@ -13,6 +15,13 @@ public class Main {
         int jazzNumber = 0;
         int rapNumber = 0;
         int rNBNumber = 0;
+        int counter = 1;
+        
+        String firstChoice = "";
+        String secondChoice = "";
+        String thirdChoice = "";
+        String fourthChoice = "";
+        String fifthChoice = "";
 
         List<String> genres = new ArrayList<>();
         genres.add("Classical");
@@ -25,6 +34,57 @@ public class Main {
         genres.add("RnB");
         genres.add("Rock");
 
+        System.out.println("Your options: ");
+        for (int i = 0; i < genres.size(); i++){
+            System.out.println(counter +  ") " + genres.get(i));
+            counter++;
+        }
+        
+        System.out.print("Enter your first choice (number): ");
+        int firstChoiceNum = s.nextInt();
+        firstChoice = genres.get(firstChoiceNum - 1);
+        genres.remove(firstChoiceNum - 1);
 
+        for (int i = 0; i < genres.size(); i++){
+            System.out.println(counter +  ") " + genres.get(i));
+            counter++;
+        }
+
+        System.out.print("Enter your second choice (number): ");
+        int secondChoiceNum = s.nextInt();
+        secondChoice = genres.get(secondChoiceNum - 1);
+        genres.remove(secondChoiceNum - 1);
+
+        for (int i = 0; i < genres.size(); i++) {
+            System.out.println(counter + ") " + genres.get(i));
+            counter++;
+        }
+
+        System.out.print("Enter your third choice (number): ");
+        int thirdChoiceNum = s.nextInt();
+        thirdChoice = genres.get(thirdChoiceNum - 1);
+        genres.remove(thirdChoiceNum - 1);
+
+        for (int i = 0; i < genres.size(); i++) {
+            System.out.println(counter + ") " + genres.get(i));
+            counter++;
+        }
+
+        System.out.print("Enter your fourth choice (number): ");
+        int fourthChoiceNum = s.nextInt();
+        fourthChoice = genres.get(fourthChoiceNum - 1);
+        genres.remove(fourthChoiceNum - 1);
+
+        for (int i = 0; i < genres.size(); i++) {
+            System.out.println(counter + ") " + genres.get(i));
+            counter++;
+        }
+
+        System.out.print("Enter your fifth choice (number): ");
+        int fifthChoiceNum = s.nextInt();
+        fifthChoice = genres.get(fifthChoiceNum - 1);
+        genres.remove(fifthChoiceNum - 1);
+        
+        
     }
 }
