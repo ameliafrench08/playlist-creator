4d295106cb3d170402a7b888eeb813a31dab92a8
Amelia French <126724144+ameliafrench08@users.noreply.github.com>
2024-11-12 10:57:31 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,12 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         Scanner s = new Scanner(System.in);
         int popNumber = 0;
         int rockNumber = 0;
@@ -24,6 +25,8 @@ public class Main {
         String fourthChoice = "";
         String fifthChoice = "";
 
+        List<Integer> numSongsPerGenre = new ArrayList<>();
+        List<String> songs = new ArrayList<>();
         List<String> genres = new ArrayList<>();
         genres.add("Classical");
         genres.add("Country");
@@ -40,64 +43,76 @@ public class Main {
             System.out.println(counter +  ") " + genres.get(i));
             counter++;
         }
-
+        counter = 1;
         System.out.print("Enter your first choice (number): ");
         int firstChoiceNum = s.nextInt();
         firstChoice = genres.get(firstChoiceNum - 1);
         genres.remove(firstChoiceNum - 1);
 
+        System.out.println(firstChoice);
+
         for (int i = 0; i < genres.size(); i++){
             System.out.println(counter +  ") " + genres.get(i));
             counter++;
         }
+        counter = 1;
 
         System.out.print("Enter your second choice (number): ");
         int secondChoiceNum = s.nextInt();
         secondChoice = genres.get(secondChoiceNum - 1);
         genres.remove(secondChoiceNum - 1);
 
+        System.out.println(secondChoice);
+
         for (int i = 0; i < genres.size(); i++) {
             System.out.println(counter + ") " + genres.get(i));
             counter++;
         }
+        counter = 1;
 
         System.out.print("Enter your third choice (number): ");
         int thirdChoiceNum = s.nextInt();
         thirdChoice = genres.get(thirdChoiceNum - 1);
         genres.remove(thirdChoiceNum - 1);
 
+        System.out.println(thirdChoice);
+
         for (int i = 0; i < genres.size(); i++) {
             System.out.println(counter + ") " + genres.get(i));
             counter++;
         }
+        counter = 1;
 
         System.out.print("Enter your fourth choice (number): ");
         int fourthChoiceNum = s.nextInt();
         fourthChoice = genres.get(fourthChoiceNum - 1);
         genres.remove(fourthChoiceNum - 1);
 
+        System.out.println(fourthChoice);
+
         for (int i = 0; i < genres.size(); i++) {
             System.out.println(counter + ") " + genres.get(i));
             counter++;
         }
+        counter = 1;
 
         System.out.print("Enter your fifth choice (number): ");
         int fifthChoiceNum = s.nextInt();
         fifthChoice = genres.get(fifthChoiceNum - 1);
         genres.remove(fifthChoiceNum - 1);
 
+        System.out.println(fifthChoice);
+
         System.out.println();
 
         System.out.print("How many songs would you like in your playlist: ");
-        if (s.nextInt() <= 50){
-            numSongs = s.nextInt();
-        }
-        else{
-            numSongs = 50;
-        }
-        
+        numSongs = Math.min(s.nextInt(), 50);
 
-        
+
+
         CreatePlaylist playlist = new CreatePlaylist(firstChoice, secondChoice, thirdChoice, fourthChoice, fifthChoice, numSongs);
+        numSongsPerGenre = playlist.assignNumEachGenre();
+        songs = playlist.choosingSongs();
+//        System.out.println(songs);
     }
 }
